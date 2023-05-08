df46484f32839a820af6172196002ca98b6bc892
typefloat <liuzekun_123@163.com>
2023-05-09 00:35:30 +0800

    添加367题题解

---
+++ leetcode/367-有效的完全平方数.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+bool isPerfectSquare(int num)
+{
+    // 在数组搜索时的最小二乘，右侧为 len - 1，但此处不需要减一
+    int left = 0, right = num;
+    long int square, mid;
+    while (left <= right)
+    {
+        mid = (right - left) / 2 + left;
+        // mid 就要使用长整形的原因是在乘法的运算过程中结果就可能出现了溢出，即要保证 mid * mid 是不会溢出的
+        square = mid * mid;
+        if (square == num)
+            return true;
+        else if (square < num)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+    int num;
+    bool target;
+
+    // case1
+    num = 16;
+    target = true;
+    cout << "Case 1: " << check(isPerfectSquare(num), target) << endl;
+
+    // case2
+    num = 14;
+    target = false;
+    cout << "Case 2: " << check(isPerfectSquare(num), target) << endl;
+}

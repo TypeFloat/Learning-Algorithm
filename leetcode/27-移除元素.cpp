40a2e5627d86b3b43776f7022e874ab4c2997c60
typefloat <liuzekun_123@163.com>
2023-05-09 20:19:17 +0800

    添加27题题解

---
+++ leetcode/27-移除元素.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val)
+{
+    int fast = 0, slow = 0;
+    while (fast < nums.size())
+    {
+        if (nums[fast] != val)
+        {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+        ++fast;
+    }
+    return slow;
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> nums, target;
+    int val;
+
+    // case1
+    nums = vector<int>{3, 2, 2, 3};
+    val = 3;
+    target = vector<int>{2, 2};
+    cout << "Case 1: " << check(vector<int>(nums.begin(), nums.begin()+removeElement(nums, val)), target) << endl;
+
+    // case2
+    nums = vector<int>{0, 1, 2, 2, 3, 0, 4, 2};
+    val = 2;
+    target = vector<int>{0, 1, 3, 0, 4};
+    cout << "Case 2: " << check(vector<int>(nums.begin(), nums.begin()+removeElement(nums, val)), target) << endl;
+}

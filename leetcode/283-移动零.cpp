3fd8ea9881e6104653f3c2a7a1f8f9d0d650f688
typefloat <liuzekun_123@163.com>
2023-05-11 09:37:20 +0800

    添加283题题解

---
+++ leetcode/283-移动零.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+void moveZeroes(vector<int>& nums)
+{
+    int slow = 0, fast = 0;
+    while (fast < nums.size())
+    {
+        if (nums[fast] != 0)
+        {
+            nums[slow++] = nums[fast];
+        }    
+        ++fast;
+    }
+    // 移动0，可以通过将后面的值赋0来实现，这样可以减少移动次数
+    while (slow < nums.size())
+    {
+        nums[slow++] = 0;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> nums, target;
+
+    // case 1
+    nums = vector<int>{0, 1, 0, 3, 12};
+    target = vector<int>{1, 3, 12, 0, 0};
+    moveZeroes(nums);
+    cout << "Case 1: " << check(target, nums) << endl;
+
+    // case 2
+    nums = vector<int>{0};
+    target = vector<int>{0};
+    moveZeroes(nums);
+    cout << "Case 2: " << check(target, nums) << endl;
+}

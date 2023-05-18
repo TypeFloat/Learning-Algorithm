331b32bfe0a27f9be48c3faaeeeae0b9aea75a40
typefloat <liuzekun_123@163.com>
2023-05-18 16:35:07 +0800

    添加54题题解

---
+++ leetcode/54-螺旋矩阵.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    int m = matrix.size(), n = matrix[0].size(), index = 0, top = 0, bottom = m - 1, left = 0, right = n - 1, row = -1, column = 0;
+    vector<int> rtn(m*n);
+    while (index < m * n)
+    {
+        // 上行
+        while (row < right && index < m * n)
+            rtn[index++] = matrix[column][++row];
+        ++top;
+        // 右列
+        while (column < bottom && index < m * n)
+            rtn[index++] = matrix[++column][row];
+        --right;
+        // 下行
+        while (row > left && index < m * n)
+            rtn[index++] = matrix[column][--row];
+        --bottom;
+        // 左列
+        while (column > top && index < m * n)
+            rtn[index++] = matrix[--column][row];
+        ++left;
+    }
+    return rtn;
+}
+
+int main(int argc, char **argv)
+{
+    vector<vector<int>> matrix;
+    vector<int> target;
+
+    // case 1
+    matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    target = {1, 2, 3, 6, 9, 8, 7, 4, 5};
+    cout << "Case 1: " << check(target, spiralOrder(matrix)) << endl;
+
+    // case 2
+    matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    target = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+    cout << "Case 2: " << check(target, spiralOrder(matrix)) << endl;
+}

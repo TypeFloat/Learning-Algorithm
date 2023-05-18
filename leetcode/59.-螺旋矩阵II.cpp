0e16be4a481200430e1c989d88117d1a016a59aa
typefloat <liuzekun_123@163.com>
2023-05-18 16:35:02 +0800

    添加59题题解

--- leetcode/59.-螺旋矩阵II.cpp
+++ leetcode/59.-螺旋矩阵II.cpp
@@ -6,36 +6,27 @@ using namespace std;
 
 vector<vector<int>> generateMatrix(int n)
 {
-    vector<vector<int>> matrix = vector<vector<int>>(n, vector<int>(n, 0));
-    int column_min = 0, row_min = 0, column, row;
-    int index = 1, flag;
-    // 偶数列和奇数列的处理不一样
-    if (n % 2 == 0)
-        flag = n * n + 1;
-    else
-        flag = n * n;
-    while (index < flag)
+    vector<vector<int>> matrix = vector<vector<int>>(n, vector<int>(n));
+    int index = 1, left = 0, top = 0, right = n - 1, bottom = n - 1, row = -1, column = 0;
+    while (index <= n * n)
     {
-        column = column_min;
-        row = row_min;
         // 上行
-        while (row < n - row_min - 1)
-            matrix[column][row++] = index++;
+        while (row < right && index <= n * n)
+            matrix[column][++row] = index++;
+        ++top;
         // 右列
-        while (column < n - column_min - 1)
-            matrix[column++][row] = index++;
+        while (column < bottom && index <= n * n)
+            matrix[++column][row] = index++;
+        --right;
         // 下行
-        while (row >= row_min + 1)
-            matrix[column][row--] = index++;
-        // 左列
-        while (column >= column_min + 1)
-            matrix[column--][row] = index++;
-        column_min++;
-        row_min++;
+        while (row > left && index <= n * n)
+            matrix[column][--row] = index++;
+        --bottom;
+        // 上行
+        while (column > top && index <= n * n)
+            matrix[--column][row] = index++;
+        ++left;
     }
-    // 奇数列的中心数单独赋值
-    if (n % 2 != 0)
-        matrix[n/2][n/2] = index;
     return matrix;
 }
 
@@ -46,11 +37,11 @@ int main(int argc, char **argv)
 
     // case 1
     n = 3;
-    target = vector<vector<int>>{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
+    target = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
     cout << "Case 1: " << check(generateMatrix(n), target) << endl;
     
     // case 2
     n = 1;
-    target = vector<vector<int>>{{1}};
+    target = {{1}};
     cout << "Case 2: " << check(generateMatrix(n), target) << endl;
 }

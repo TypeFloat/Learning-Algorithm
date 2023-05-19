af2ff07cf9163712b719455d0b73f0d7c36ac3d6
typefloat <liuzekun_123@163.com>
2023-05-19 13:43:45 +0800

    添加ListNode的定义

--- leetcode/utils.h
+++ leetcode/utils.h
@@ -3,3 +3,12 @@ bool check(const T& value1, const T& value2)
 {
     return value1 == value2;
 }
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};

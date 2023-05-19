346f769f8206d4380d4bfe2a6f85609457cae450
typefloat <liuzekun_123@163.com>
2023-05-19 14:16:54 +0800

    添加单链表的通用函数

--- leetcode/utils.h
+++ leetcode/utils.h
@@ -18,3 +18,37 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+void createLink(vector<int> &nums, ListNode *head)
+{
+    ListNode *ptr = head;
+    for (int num : nums)
+    {
+        ptr->next = new ListNode(num);
+        ptr = ptr->next;
+    }
+}
+
+void deleteLink(ListNode *head)
+{
+    ListNode *ptr = head->next, *tmp;
+    while (ptr != nullptr)
+    {
+        tmp = ptr;
+        ptr = ptr->next;
+        delete tmp;
+    }
+    head->next = nullptr;
+}
+
+vector<int> link2vector(ListNode *head)
+{
+    vector<int> rtn;
+    ListNode *ptr = head->next;
+    while (ptr != nullptr)
+    {
+        rtn.emplace_back(ptr->val);
+        ptr = ptr->next;
+    }
+    return rtn;
+}

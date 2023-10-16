04edbd8f49722d9321bd4403e836f8abcfbb233f
typefloat <liuzekun_123@163.com>
2023-10-16 09:28:01 +0800

    修改使用google test进行测试

--- leetcode/704-二分查找.cpp
+++ leetcode/704-二分查找.cpp
@@ -1,13 +1,11 @@
-#include "utils.h"
+#include <gtest/gtest.h>
 
 using namespace std;
 
-int search(vector<int> &nums, int target)
-{
+int search(vector<int> &nums, int target) {
     // 不能用 unsigned int，因为 right 可能会小于 0
     int left = 0, right = nums.size() - 1, mid;
-    while (left <= right)
-    {
+    while (left <= right) {
         // 等同于 mid = (left + right) / 2，这样可以防止 left + right 数据溢出
         mid = (right - left) / 2 + left;
         if (nums[mid] < target)
@@ -20,26 +18,14 @@ int search(vector<int> &nums, int target)
     return -1;
 }
 
-void solution(vector<int> &nums, int target, int output)
-{
-    check(search(nums, target), output);
+TEST(TEST704, CASE1) {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    ASSERT_EQ(search(nums, target), 4);
 }
 
-int main(int argc, char **argv)
-{
-    vector<int> nums;
-    int target;
-    int output;
-
-    // case 1
-    nums = {-1, 0, 3, 5, 9, 12};
-    target = 9;
-    output = 4;
-    solution(nums, target, output);
-
-    // case 2
-    nums = {-1, 0, 3, 5, 9, 12};
-    target = 2;
-    output = -1;
-    solution(nums, target, output);
+TEST(TEST704, CASE2) {
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 2;
+    ASSERT_EQ(search(nums, target), -1);
 }

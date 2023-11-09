2a486f48fff8b38b4b17a662073046895cc35d78
typefloat <liuzekun_123@163.com>
2023-11-09 09:20:52 +0800

    更新34题题解

--- leetcode/34.在排序数组中查询元素的第一个和最后一个位置.cpp
+++ leetcode/34.在排序数组中查询元素的第一个和最后一个位置.cpp
@@ -1,73 +1,72 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int binarySearch(vector<int> &nums, int target, int left, int right);
-
-vector<int> searchRange(vector<int> &nums, int target)
-{
-    int left = nums.size(), right = -1, low = 0, high = nums.size() - 1;
-    // 使用的思路是不断在一个缩小的区间里寻找目标，每一次查找都是一次二分查找。
-    do
-    {
-        low = binarySearch(nums, target, 0, left - 1);
-        if (low != -1)
-            left = low;
-    } while (low != -1);
-    do
-    {
-        high = binarySearch(nums, target, right + 1, nums.size() - 1);
-        if (high != -1)
-            right = high;
-    } while (high != -1);
-    // 如果 right = -1 说明 nums 里没有 target，则返回 {-1, -1}
-    if (right < 0)
-        return {-1, -1};
-    else
-        return {left, right};
-}
-
-int binarySearch(vector<int> &nums, int target, int left, int right)
-{
+int binary_search(vector<int> &nums, int target, int left, int right) {
+    if (left > right) return -1;
     int mid;
-    while (left <= right)
-    {
-        mid = (right - left) / 2 + left;
-        if (nums[mid] < target)
-            left = mid + 1;
-        else if (nums[mid] > target)
-            right = mid - 1;
-        else
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (nums[mid] == target)
             return mid;
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
-    return -1;
+    return nums[left] == target ? left : -1;
+}
+vector<int> searchRange(vector<int> &nums, int target) {
+    // 寻找是否存在目标值，如果没有，直接结束
+    int left = binary_search(nums, target, 0, nums.size() - 1);
+    if (left == -1) return {-1, -1};
+    int right = left;
+    int index;
+    // 寻找边界的过程就是在子区间内寻找目标值，若有，就修改边界，没有就结束寻找
+    // 寻找左边界
+    index = left - 1;
+    while (index >= 0) {
+        index = binary_search(nums, target, 0, index);
+        if (index != -1) {
+            left = index;
+            --index;
+        }
+    }
+    // 寻找右边界
+    index = right + 1;
+    while (index >= 0) {
+        index = binary_search(nums, target, index, nums.size() - 1);
+        if (index != -1) {
+            right = index;
+            ++index;
+        }
+    }
+    return {left, right};
 }
 
-void solution(vector<int> &nums, vector<int> &output, int target)
-{
-    check(searchRange(nums, target), output);
+TEST(Q34, CASE1) {
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    vector<int> outputs = {3, 4};
+    ASSERT_EQ(searchRange(nums, 8), outputs);
 }
 
-int main(int argc, char **argv)
-{
-    vector<int> nums, output;
-    int target;
+TEST(Q34, CASE2) {
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    vector<int> outputs = {-1, -1};
+    ASSERT_EQ(searchRange(nums, 6), outputs);
+}
 
-    // case 1
-    nums = {5, 7, 7, 8, 8, 10};
-    output = {3, 4};
-    target = 8;
-    solution(nums, output, target);
+TEST(Q34, CASE3) {
+    vector<int> nums = {};
+    vector<int> outputs = {-1, -1};
+    ASSERT_EQ(searchRange(nums, 0), outputs);
+}
 
-    // case 2
-    nums = {5, 7, 7, 8, 8, 10};
-    output = {-1, -1};
-    target = 6;
-    solution(nums, output, target);
-
-    // case 3
-    nums = {};
-    output = {-1, -1};
-    target = 0;
-    solution(nums, output, target);
+TEST(Q34, CASE4) {
+    vector<int> nums = {1};
+    vector<int> outputs = {0, 0};
+    ASSERT_EQ(searchRange(nums, 1), outputs);
 }

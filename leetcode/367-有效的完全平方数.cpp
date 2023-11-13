4fc2f75dc14ed7a839712be46fca7c4082769bff
typefloat <liuzekun_123@163.com>
2023-11-14 07:38:46 +0800

    更新367题题解

--- leetcode/367-有效的完全平方数.cpp
+++ leetcode/367-有效的完全平方数.cpp
@@ -1,18 +1,14 @@
-#include "utils.h"
+#include <gtest/gtest.h>
 
-bool isPerfectSquare(int num)
-{
-    // 在数组搜索时的最小二乘，右侧为 len - 1，但此处不需要减一
+bool isPerfectSquare(int num) {
+    long long squd, mid;
     int left = 0, right = num;
-    long int square, mid;
-    while (left <= right)
-    {
-        mid = (right - left) / 2 + left;
-        // mid 就要使用长整形的原因是在乘法的运算过程中结果就可能出现了溢出，即要保证 mid * mid 是不会溢出的
-        square = mid * mid;
-        if (square == num)
+    while (left <= right) {
+        mid = (left + right) / 2;
+        squd = mid * mid;
+        if (squd == num)
             return true;
-        else if (square < num)
+        else if (squd < num)
             left = mid + 1;
         else
             right = mid - 1;
@@ -20,23 +16,6 @@ bool isPerfectSquare(int num)
     return false;
 }
 
-void solution(int num, bool output)
-{
-    check(isPerfectSquare(num), output);
-}
+TEST(Q367, CASE1) { ASSERT_EQ(isPerfectSquare(16), true); }
 
-int main(int argc, char **argv)
-{
-    int num;
-    bool target;
-
-    // case1
-    num = 16;
-    target = true;
-    solution(num, target);
-
-    // case2
-    num = 14;
-    target = false;
-    solution(num, target);
-}
+TEST(Q367, CASE2) { ASSERT_EQ(isPerfectSquare(14), false); }

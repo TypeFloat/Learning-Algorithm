d250389f6bff567fd63d639dbf24d407350ef01d
typefloat <liuzekun_123@163.com>
2023-11-21 09:32:32 +0800

    更新283题解

--- leetcode/283-移动零.cpp
+++ leetcode/283-移动零.cpp
@@ -1,42 +1,32 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+
+#include <vector>
 
 using namespace std;
 
-void moveZeroes(vector<int> &nums)
-{
+void moveZeroes(vector<int> &nums) {
     int slow = 0, fast = 0;
-    while (fast < nums.size())
-    {
-        if (nums[fast] != 0)
-        {
+    while (fast < nums.size()) {
+        if (nums[fast] != 0) {
             nums[slow++] = nums[fast];
-        }
+        };
         ++fast;
     }
-    // 移动0，可以通过将后面的值赋0来实现，这样可以减少移动次数
-    while (slow < nums.size())
-    {
-        nums[slow++] = 0;
+    for (; slow < nums.size(); ++slow) {
+        nums[slow] = 0;
     }
 }
 
-void solution(vector<int> &nums, vector<int> &target)
-{
+TEST(Q283, CASE1) {
+    vector<int> nums = {0, 1, 0, 3, 12};
+    vector<int> target = {1, 3, 12, 0, 0};
     moveZeroes(nums);
-    check(target, nums);
+    ASSERT_EQ(nums, target);
 }
 
-int main(int argc, char **argv)
-{
-    vector<int> nums, target;
-
-    // case 1
-    nums = {0, 1, 0, 3, 12};
-    target = {1, 3, 12, 0, 0};
-    solution(nums, target);
-
-    // case 2
-    nums = {0};
-    target = {0};
-    solution(nums, target);
+TEST(Q283, CASE2) {
+    vector<int> nums = {0};
+    vector<int> target = {0};
+    moveZeroes(nums);
+    ASSERT_EQ(nums, target);
 }

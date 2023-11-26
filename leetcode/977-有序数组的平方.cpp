24ddf5706699f11dda481e3d9f3ab2d85b73847a
typefloat <liuzekun_123@163.com>
2023-11-26 13:55:40 +0800

    更新977题解

--- leetcode/977-有序数组的平方.cpp
+++ leetcode/977-有序数组的平方.cpp
@@ -1,48 +1,33 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+
+#include <vector>
 
 using namespace std;
 
-vector<int> sortedSquares(vector<int> &nums)
-{
-    if (nums.size() == 1)
-        return {nums[0] * nums[0]};
+vector<int> sortedSquares(vector<int> &nums) {
+    int left = 0, right = nums.size() - 1;
     vector<int> rtn(nums.size());
-    // 解体策略是从两头向中间走，而不是从中间向两头走，因为不好确定中间元素的位置
-    int left = 0, right = nums.size() - 1, index = rtn.size() - 1, leftSquare, rightSquare;
-    while (index >= 0)
-    {
-        leftSquare = nums[left] * nums[left];
-        rightSquare = nums[right] * nums[right];
-        if (leftSquare < rightSquare)
-        {
-            rtn[index--] = rightSquare;
-            --right;
-        }
-        else
-        {
-            rtn[index--] = leftSquare;
+    int l, r;
+    for (int i = nums.size() - 1; i >= 0; --i) {
+        l = nums[left] * nums[left];
+        r = nums[right] * nums[right];
+        if (l >= r) {
+            rtn[i] = l;
             ++left;
+        } else {
+            rtn[i] = r;
+            --right;
         }
     }
     return rtn;
 }
 
-void solution(vector<int> &nums, vector<int> &target)
-{
-    check(sortedSquares(nums), target);
+TEST(Q977, CASE1) {
+    vector<int> nums = {-4, -1, 0, 3, 10}, target = {0, 1, 9, 16, 100};
+    ASSERT_EQ(sortedSquares(nums), target);
 }
 
-int main(int argc, char **argv)
-{
-    vector<int> nums, target;
-
-    // case 1
-    nums = {-4, -1, 0, 3, 10};
-    target = {0, 1, 9, 16, 100};
-    solution(nums, target);
-
-    // case 2
-    nums = {-7, -3, 2, 3, 11};
-    target = {4, 9, 9, 49, 121};
-    solution(nums, target);
+TEST(Q977, CASE2) {
+    vector<int> nums = {-7, -3, 2, 3, 11}, target = {4, 9, 9, 49, 121};
+    ASSERT_EQ(sortedSquares(nums), target);
 }

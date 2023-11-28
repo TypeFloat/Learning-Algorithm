2bed8027557562a9a532cbbf7c861930a7517739
typefloat <liuzekun_123@163.com>
2023-11-29 00:10:51 +0800

    更新59题解

--- leetcode/59.-螺旋矩阵II.cpp
+++ leetcode/59.-螺旋矩阵II.cpp
@@ -1,50 +1,59 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+
+#include <vector>
 
 using namespace std;
 
-vector<vector<int>> generateMatrix(int n)
-{
-    vector<vector<int>> matrix = vector<vector<int>>(n, vector<int>(n));
-    int index = 1, left = 0, top = 0, right = n - 1, bottom = n - 1, row = -1, column = 0;
-    while (index <= n * n)
-    {
-        // 上行
-        while (row < right && index <= n * n)
-            matrix[column][++row] = index++;
-        ++top;
-        // 右列
-        while (column < bottom && index <= n * n)
-            matrix[++column][row] = index++;
+vector<vector<int>> generateMatrix(int n) {
+    vector<vector<int>> matrix(n, vector<int>(n));
+    int up = 0, down = n - 1, left = 0, right = n - 1;
+    int index = 1;
+    while (up < down && left < right) {
+        // 处理上边界
+        for (int i = left; i < right; ++i) {
+            matrix[up][i] = index++;
+        }
+        // 处理右边界
+        for (int i = up; i < down; ++i) {
+            matrix[i][right] = index++;
+        }
+        // 处理下边界
+        for (int i = right; i > left; --i) {
+            matrix[down][i] = index++;
+        }
+        // 处理左边届
+        for (int i = down; i > up; --i) {
+            matrix[i][left] = index++;
+        }
+        ++up;
         --right;
-        // 下行
-        while (row > left && index <= n * n)
-            matrix[column][--row] = index++;
-        --bottom;
-        // 上行
-        while (column > top && index <= n * n)
-            matrix[--column][row] = index++;
+        --down;
         ++left;
+    }
+    if (n % 2 != 0) {
+        int ptr = (n - 1) / 2;
+        matrix[ptr][ptr] = index;
     }
     return matrix;
 }
 
-void solution(int n, vector<vector<int>> &target)
-{
-    check(generateMatrix(n), target);
+TEST(Q59, CASE1) {
+    vector<vector<int>> target = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
+    ASSERT_EQ(generateMatrix(3), target);
 }
 
-int main(int argc, char **argv)
-{
-    int n;
-    vector<vector<int>> target;
+TEST(Q59, CASE2) {
+    vector<vector<int>> target = {{1}};
+    ASSERT_EQ(generateMatrix(1), target);
+}
 
-    // case 1
-    n = 3;
-    target = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
-    solution(n, target);
+TEST(Q59, CASE3) {
+    vector<vector<int>> target = {{1, 2}, {4, 3}};
+    ASSERT_EQ(generateMatrix(2), target);
+}
 
-    // case 2
-    n = 1;
-    target = {{1}};
-    solution(n, target);
+TEST(Q59, CASE4) {
+    vector<vector<int>> target = {
+        {1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
+    ASSERT_EQ(generateMatrix(4), target);
 }

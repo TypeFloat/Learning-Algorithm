90155ef4dc43ee8f8a82ef1d4bcf69ea2984db41
typefloat <liuzekun_123@163.com>
2023-11-29 00:55:07 +0800

    更新54题解

--- leetcode/54-螺旋矩阵.cpp
+++ leetcode/54-螺旋矩阵.cpp
@@ -1,50 +1,66 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+#include <sys/syslimits.h>
+
+#include <vector>
 
 using namespace std;
 
-vector<int> spiralOrder(vector<vector<int>> &matrix)
-{
-    int m = matrix.size(), n = matrix[0].size(), index = 0, top = 0, bottom = m - 1, left = 0, right = n - 1, row = -1, column = 0;
-    vector<int> rtn(m * n);
-    while (index < m * n)
-    {
-        // 上行
-        while (row < right && index < m * n)
-            rtn[index++] = matrix[column][++row];
-        ++top;
-        // 右列
-        while (column < bottom && index < m * n)
-            rtn[index++] = matrix[++column][row];
+vector<int> spiralOrder(vector<vector<int>> &matrix) {
+    int up = 0, down = matrix.size() - 1, left = 0,
+        right = matrix[0].size() - 1;
+    int index = 0;
+    vector<int> rtn(matrix.size() * matrix[0].size());
+    while (up < down && left < right) {
+        // 处理上边界
+        for (int i = left; i < right; ++i) {
+            rtn[index++] = matrix[up][i];
+        }
+        // 处理右边界
+        for (int i = up; i < down; ++i) {
+            rtn[index++] = matrix[i][right];
+        }
+        // 处理下边界
+        for (int i = right; i > left; --i) {
+            rtn[index++] = matrix[down][i];
+        }
+        // 处理左边届
+        for (int i = down; i > up; --i) {
+            rtn[index++] = matrix[i][left];
+        }
+        ++up;
         --right;
-        // 下行
-        while (row > left && index < m * n)
-            rtn[index++] = matrix[column][--row];
-        --bottom;
-        // 左列
-        while (column > top && index < m * n)
-            rtn[index++] = matrix[--column][row];
+        --down;
         ++left;
+    }
+    if (index > rtn.size() - 1) return rtn;
+    if (up == down) {
+        for (int i = left; i <= right; ++i) {
+            rtn[index++] = matrix[up][i];
+        }
+    } else {
+        for (int i = up; i <= down; ++i) {
+            rtn[index++] = matrix[i][left];
+        }
     }
     return rtn;
 }
 
-void solution(vector<vector<int>> &matrix, vector<int> &target)
-{
-    check(spiralOrder(matrix), target);
+TEST(Q54, CASE1) {
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<int> target = {1, 2, 3, 6, 9, 8, 7, 4, 5};
+    ASSERT_EQ(spiralOrder(matrix), target);
 }
 
-int main(int argc, char **argv)
-{
-    vector<vector<int>> matrix;
-    vector<int> target;
+TEST(Q54, CASE2) {
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    vector<int> target = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+    ASSERT_EQ(spiralOrder(matrix), target);
+}
 
-    // case 1
-    matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    target = {1, 2, 3, 6, 9, 8, 7, 4, 5};
-    solution(matrix, target);
-
-    // case 2
-    matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-    target = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
-    solution(matrix, target);
+TEST(Q54, CASE3) {
+    vector<vector<int>> matrix = {{1, 11}, {2, 12}, {3, 13}, {4, 14}, {5, 15},
+                                  {6, 16}, {7, 17}, {8, 18}, {9, 19}, {10, 20}};
+    vector<int> target = {1,  11, 12, 13, 14, 15, 16, 17, 18, 19,
+                          20, 10, 9,  8,  7,  6,  5,  4,  3,  2};
+    ASSERT_EQ(spiralOrder(matrix), target);
 }

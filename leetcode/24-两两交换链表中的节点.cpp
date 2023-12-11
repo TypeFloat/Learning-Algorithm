b382821c162b52c065fa1667362cee9de6dc94f6
typefloat <liuzekun_123@163.com>
2023-12-11 23:45:12 +0800

    更新24题解

--- leetcode/24-两两交换链表中的节点.cpp
+++ leetcode/24-两两交换链表中的节点.cpp
@@ -1,44 +1,46 @@
-#include "utils.h"
+#include <gtest/gtest.h>
+
+#include <vector>
+
 #include "link.h"
 
 using namespace std;
 
-ListNode *swapPairs(ListNode *head)
-{
-    if (head == nullptr || head->next == nullptr)
-        return head;
-    ListNode *preNode = head, *nextNode = head->next;
-    preNode->next = nextNode->next;
-    nextNode->next = preNode;
-    preNode->next = swapPairs(preNode->next);
-    return nextNode;
+ListNode *swapPairs(ListNode *head) {
+    if (head == nullptr) return nullptr;
+    ListNode *dummy_head = new ListNode();
+    dummy_head->next = head;
+    ListNode *ptr1 = dummy_head, *ptr2 = head, *ptr3 = head->next;
+    while (ptr2 && ptr3) {
+        ptr1->next = ptr3;
+        ptr2->next = ptr3->next; 
+        ptr3->next = ptr2;
+        ptr1 = ptr2;
+        ptr2 = ptr1->next;
+        if (ptr2 == nullptr) break;
+        ptr3 = ptr1->next->next;
+    }
+    return dummy_head->next;
 }
 
-void solution(vector<int> nums, vector<int> target)
-{
-    LinkList linkList;
-    linkList.createLink(nums);
-    linkList.head->next = swapPairs(linkList.head->next);
-    check(linkList.toVector(), target);
-    linkList.deleteLink();
+void judge(vector<int> &nums, vector<int> &target) {
+    LinkList *link_list = new LinkList();
+    link_list->createLink(nums);
+    link_list->head->next = swapPairs(link_list->head->next);
+    ASSERT_TRUE(link_list->toVector() == target);
 }
 
-int main(int argc, char **argv)
-{
-    vector<int> nums, target;
+TEST(Q24, CASE1) {
+    vector<int> nums = {1, 2, 3, 4}, target = {2, 1, 4, 3};
+    judge(nums, target);
+}
 
-    // case 1
-    nums = {1, 2, 3, 4};
-    target = {2, 1, 4, 3};
-    solution(nums, target);
+TEST(Q24, CASE2) {
+    vector<int> nums = {}, target = {};
+    judge(nums, target);
+}
 
-    // case 2
-    nums = {};
-    target = {};
-    solution(nums, target);
-
-    // case 3
-    nums = {1};
-    target = {1};
-    solution(nums, target);
+TEST(Q24, CASE3) {
+    vector<int> nums = {1}, target = {1};
+    judge(nums, target);
 }

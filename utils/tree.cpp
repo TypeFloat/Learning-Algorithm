ae8ee1189470a440ae140460acb37c7426e000cd
typefloat <liuzekun_123@163.com>
2024-06-01 20:46:48 +0800

    修改测试文件与扩展数据结构

--- utils/tree.cpp
+++ utils/tree.cpp
@@ -1,21 +1,19 @@
 #include "tree.h"
 
-BinaryTree::BinaryTree(std::vector<int> &val_list)
-{
-    if (val_list.size() == 0)
-    {
+#include <stack>
+
+BinaryTree::BinaryTree(std::vector<int> &val_list) {
+    if (val_list.size() == 0) {
         this->root = nullptr;
         return;
     }
     std::stack<TreeNode **> my_stack;
     my_stack.push(&this->root);
     TreeNode **ptr;
-    for (int val : val_list)
-    {
+    for (int val : val_list) {
         ptr = my_stack.top();
         my_stack.pop();
-        if (val != -1)
-        {
+        if (val != -1) {
             *ptr = new TreeNode(val);
             my_stack.push(&((*ptr)->right));
             my_stack.push(&((*ptr)->left));
@@ -23,21 +21,16 @@ BinaryTree::BinaryTree(std::vector<int> &val_list)
     }
 }
 
-BinaryTree::~BinaryTree()
-{
-    if (this->root == nullptr)
-        return;
+BinaryTree::~BinaryTree() {
+    if (this->root == nullptr) return;
     std::stack<TreeNode *> my_stack;
     my_stack.push(this->root);
     TreeNode *ptr;
-    while (!my_stack.empty())
-    {
+    while (!my_stack.empty()) {
         ptr = my_stack.top();
         my_stack.pop();
-        if (ptr->left != nullptr)
-            my_stack.push(ptr->left);
-        if (ptr->right != nullptr)
-            my_stack.push(ptr->right);
+        if (ptr->left != nullptr) my_stack.push(ptr->left);
+        if (ptr->right != nullptr) my_stack.push(ptr->right);
         delete ptr;
     }
 }
--- utils/tree.h
+++ utils/tree.h
@@ -1,36 +1,33 @@
-#include "utils.h"
+#include <vector>
 
-class TreeNode
-{
-public:
+class TreeNode {
+   public:
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 
 class Node {
-public:
+   public:
     int val;
-    std::vector<Node*> children;
+    std::vector<Node *> children;
 
     Node() {}
 
-    Node(int _val) {
-        val = _val;
-    }
+    Node(int _val) { val = _val; }
 
-    Node(int _val, std::vector<Node*> _children) {
+    Node(int _val, std::vector<Node *> _children) {
         val = _val;
         children = _children;
     }
 };
 
-class BinaryTree
-{
-public:
+class BinaryTree {
+   public:
     TreeNode *root;
     BinaryTree(std::vector<int> &val_list);
     ~BinaryTree();
